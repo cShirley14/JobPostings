4fc51187b6d7a63a6a904af4179896eb31dac982
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2021-04-28 11:24:37 -0500

    Initial implementation of application and attachment classes

---
+++ src/main/java/io/shirley/domainmodels/Application.java
@@ -0,0 +1,208 @@
+package io.shirley.domainmodels;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Comparator;
+
+/**
+ *
+ * @author Shirley
+ */
+public class Application implements Comparator<Application> {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Attachment attachment;
+    private double desiredSalary;
+    private LocalDate earliestStartDate;
+    private int id;
+    private int jobId;
+    private Instant dateTimeSubmitted;
+    private boolean active;
+    private String firstNameError;
+    private String lastNameError;
+    private String emailError;
+    private String phoneError;
+    private String resumeError;
+    private String startDateError;
+    private String invalidDataError;
+
+    public Application() {
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.phone = "";
+        this.attachment = null;
+        this.desiredSalary = 0;
+        this.earliestStartDate = null;
+        this.id = 0;
+        this.jobId = 0;
+        this.dateTimeSubmitted = null;
+        this.active = false;
+        this.firstNameError = "";
+        this.lastNameError = "";
+        this.emailError = "";
+        this.phoneError = "";
+        this.resumeError = "";
+        this.startDateError = "";
+        this.invalidDataError = "";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Attachment getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(Attachment attachment) {
+        this.attachment = attachment;
+    }
+
+    public double getDesiredSalary() {
+        return desiredSalary;
+    }
+
+    public void setDesiredSalary(double desiredSalary) {
+        this.desiredSalary = desiredSalary;
+    }
+
+    public LocalDate getEarliestStartDate() {
+        return earliestStartDate;
+    }
+
+    public void setEarliestStartDate(LocalDate earliestStartDate) {
+        this.earliestStartDate = earliestStartDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    public Instant getDateTimeSubmitted() {
+        return dateTimeSubmitted;
+    }
+
+    public void setDateTimeSubmitted(Instant dateTimeSubmitted) {
+        this.dateTimeSubmitted = dateTimeSubmitted;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFirstNameError() {
+        return firstNameError;
+    }
+
+    public void setFirstNameError(String firstNameError) {
+        this.firstNameError = firstNameError;
+    }
+
+    public String getLastNameError() {
+        return lastNameError;
+    }
+
+    public void setLastNameError(String lastNameError) {
+        this.lastNameError = lastNameError;
+    }
+
+    public String getEmailError() {
+        return emailError;
+    }
+
+    public void setEmailError(String emailError) {
+        this.emailError = emailError;
+    }
+
+    public String getPhoneError() {
+        return phoneError;
+    }
+
+    public void setPhoneError(String phoneError) {
+        this.phoneError = phoneError;
+    }
+
+    public String getResumeError() {
+        return resumeError;
+    }
+
+    public void setResumeError(String resumeError) {
+        this.resumeError = resumeError;
+    }
+
+    public String getStartDateError() {
+        return startDateError;
+    }
+
+    public void setStartDateError(String startDateError) {
+        this.startDateError = startDateError;
+    }
+
+    public String getInvalidDataError() {
+        return invalidDataError;
+    }
+
+    public void setInvalidDataError(String invalidDataError) {
+        this.invalidDataError = invalidDataError;
+    }
+
+    @Override
+    public String toString() {
+        return "Application{" + "firstName=" + firstName + ", lastName=" + lastName + '}';
+    }
+
+    @Override
+    public int compare(Application o1, Application o2) {
+        // Will Implement
+        return 0;
+    }
+    
+    
+}
