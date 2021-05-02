9108ccf5970ff4aac3d1940a5498ef213654bb92
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2021-05-01 19:34:53 -0500

    Working on servlet

--- src/main/java/io/shirley/ApplicationServlet.java
+++ src/main/java/io/shirley/ApplicationServlet.java
@@ -6,9 +6,14 @@
 package io.shirley;
 
 import io.shirley.domainmodels.Application;
+import io.shirley.domainmodels.Attachment;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.ServletException;
@@ -16,6 +21,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  *
@@ -23,6 +29,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ApplicationServlet", urlPatterns = {"/applications"})
 public class ApplicationServlet extends HttpServlet {
+    private volatile int APPLICATION_ID_SEQUENCE = 1;
+    List<Application> apps = new ArrayList<Application>();
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -34,7 +43,7 @@ public class ApplicationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
     /**
@@ -52,7 +61,7 @@ public class ApplicationServlet extends HttpServlet {
         if (action == null) {
             action = "defaultList";
         }
-        
+
         switch (action) {
             case "defaultList":
                 viewList(request, response);
@@ -64,25 +73,24 @@ public class ApplicationServlet extends HttpServlet {
                 viewList(request, response);
                 break;
         }
-        
+
     }
 
     /**
-     * Returns user to the job list page if they submit
-     * an invalid POST request.
-     * 
-     * Returns 
+     * Returns user to the job list page if they submit an invalid POST request.
+     *
+     * Returns
+     *
      * @param request
      * @param response
      * @throws ServletException
-     * @throws IOException 
+     * @throws IOException
      */
     private void viewList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         request.getRequestDispatcher("/WEB-INF/jsp/view/jobList.jsp")
-               .forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/jsp/view/jobList.jsp")
+                .forward(request, response);
     }
 
-    
     private void submitJobApplication(HttpServletRequest request, HttpServletResponse response) {
         String action = request.getParameter("action");
         Application app = new Application();
@@ -93,37 +101,40 @@ public class ApplicationServlet extends HttpServlet {
         // Pattern from Oreilly Java Cookbook: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s01.html
         // and https://www.baeldung.com/java-regex-validate-phone-numbers
         Pattern emailPattern = Pattern.compile("\\A[\\w!#$%&'*+/=?`{|}~^-]+"
-                + "(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@↵\n" +
-                "(?:[A-Z0-9-]+\\.)+[A-Z]{2,6}\\Z");
+                + "(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@↵\n"
+                + "(?:[A-Z0-9-]+\\.)+[A-Z]{2,6}\\Z");
         String phone;
         Pattern phonePattern = Pattern.compile("^(\\d{3}[- .]?){2}\\d{4}$");
         double desiredSalary;
-        
+        int id;
+        int jobId;
+        boolean active;
+
         try {
-            if (request.getParameter("firstName") != null && 
-                    !request.getParameter("firstName").equals("")) {
+            if (request.getParameter("firstName") != null
+                    && !request.getParameter("firstName").equals("")) {
                 firstName = request.getParameter("firstName");
             } else {
                 app.setFirstNameError(true);
             }
-            if (request.getParameter("lastName") != null && 
-                    !request.getParameter("lastName").equals("")) {
+            if (request.getParameter("lastName") != null
+                    && !request.getParameter("lastName").equals("")) {
                 lastName = request.getParameter("firstName");
             } else {
                 app.setLastNameError(true);
             }
             Matcher emailMatcher = emailPattern.matcher(request.getParameter("email"));
-            if (request.getParameter("email") != null &&
-                    !request.getParameter("email").equals("") &&
-                    emailMatcher.matches()) {
+            if (request.getParameter("email") != null
+                    && !request.getParameter("email").equals("")
+                    && emailMatcher.matches()) {
                 email = request.getParameter("email");
             } else {
                 app.setEmailError(true);
             }
             Matcher phoneMatcher = phonePattern.matcher(request.getParameter("phone"));
-            if (request.getParameter("phone") != null &&
-                    !request.getParameter("phone").equals("") &&
-                    phoneMatcher.matches()) {
+            if (request.getParameter("phone") != null
+                    && !request.getParameter("phone").equals("")
+                    && phoneMatcher.matches()) {
                 phone = request.getParameter("phone");
             } else {
                 app.setPhoneError(true);
@@ -134,26 +145,72 @@ public class ApplicationServlet extends HttpServlet {
                 app.setSalaryError(true);
             }
             try {
-                
+                earliestStartDate = LocalDate.parse(request.getParameter("earliestStartDate"));
+            } catch (Exception ex) {
+                app.setStartDateError(true);
             }
-            
-            
-            // Convert Double
-            String desiredSalary = request.getParameter("desiredSalary");
-            // Convert Date
-            String earliestStartDate = request.getParameter("earliestStartDate");
-            // convert int
-            String id = request.getParameter("id");
-            // convert int
-            String jobId = request.getParameter("jobId");
-            // Get date time submitted
-            // Convert bool
-            String active = request.getParameter("active");
-            
+            try {
+                jobId = Integer.parseInt(request.getParameter("jobId"));
+            } catch (Exception ex) {
+                app.setInvalidDataError(true);
+            }
+            try {
+                active = Boolean.parseBoolean(request.getParameter("active"));
+            } catch (Exception ex) {
+                app.setInvalidDataError(true);
+            }
+            // Upload attachment
+            try {
+                Part filePart = request.getPart("attachment");
+                if (filePart != null && filePart.getSize() > 0) {
+                    Attachment attachment = this.processAttachment(filePart);
+                    if (attachment != null) {
+                        app.setAttachment(attachment);
+                    }
+                }
+            } catch (Exception ex) {
+                app.setResumeError(true);
+            }
+
+            if (app.getEmailError() == true || app.getFirstNameError() == true
+                    || app.getInvalidDataError() == true || app.getLastNameError() == true
+                    || app.getPhoneError() == true || app.getResumeError() == true
+                    || app.getStartDateError() == true) {
+                // Redirect to application again to show error message(s).
+                request.getRequestDispatcher("/WEB-INF/jsp/view/job.jsp");
+            } else {
+                // Set application ID
+                synchronized (this) {
+                    id = this.APPLICATION_ID_SEQUENCE++;
+                    app.setId(id);
+                }
+                // add successful job application
+                apps.add(app);
+                // Show success message on same page
+                request.getRequestDispatcher("/WEB-INF/jsp/view/job.jsp");
+            }
         } catch (Exception ex) {
-            // show errors
+            request.getRequestDispatcher("/WEB-INF/jsp/view/job.jsp");
         }
     }
 
+    private Attachment processAttachment(Part filePart) throws IOException {
+        InputStream inputStream = filePart.getInputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        int read;
+        final byte[] bytes = new byte[1024];
+
+        while((read = inputStream.read(bytes)) != -1)
+        {
+            outputStream.write(bytes, 0, read);
+        }
+
+        Attachment attachment = new Attachment();
+        attachment.setName(filePart.getSubmittedFileName());
+        attachment.setContents(outputStream.toByteArray());
+
+        return attachment;
+    }
 
 }
